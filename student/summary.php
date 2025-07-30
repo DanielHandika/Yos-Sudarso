5de046b64b437eb2254495121d0b64ec7a915166
DanielHandika <danielkristih.p@gmail.com>
2025-07-30 23:10:22 +0700

    Change style

--- student/summary.php
+++ student/summary.php
@@ -226,7 +226,7 @@ if($b>0){
                                                 <td><?php echo number_format($rowa['bill']);?></td>
                                                  <td><?php echo number_format($rowa['sudahbayar']);?></td>
                                                  <?php if($rowa['status']!='belum'){?>
-                                                <td class="table-danger"><?php echo $rowa['status'];?></td>
+                                                <td class="table-success" style="color: black;"><?php echo $rowa['status'];?></td>
                                             <?php } else {?>
                                                     <td ><?php echo $rowa['status'];?></td>
                                             <?php } ?>
